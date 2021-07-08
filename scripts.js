03cf3a08ad5d4b549f3fda1fb7577790b30c43fd
Lasya Vanga <lasya@lasya.net>
2021-07-08 13:41:14 -0700

    Added Sign Finder

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
-let num;
+let num = 0;
 
 const inputEl = document.getElementById("input");
 const nanEl = document.getElementById("nan");
-const ianEl = document.getElementById("ian");
 const posEl = document.getElementById("pos");
 const negEl = document.getElementById("neg");
 const zeroEl = document.getElementById("zero");
@@ -11,12 +10,37 @@ const numEl = document.getElementById("number");
 
 if (numEl === "") {
   inputEl.style.backgroundColor = "rgba(255, 53, 22, 0.5)";
+  // } else if () {
 }
 
-numEl.addEventListener("click", () => {
-  inputEl.style.backgroundColor = "rgba(255, 53, 22, 0.5)";
+numEl.addEventListener("keyup", () => {
+  // Remove selected class
+  document.querySelectorAll(".selected").forEach((el) => {
+    el.classList.remove("selected");
+  });
+
+  // Get current value
+  const valStr = numEl.value;
+
+  // Check for each item
+
+  if (valStr === "") {
+    inputEl.classList.add("selected");
+  } else {
+    const val = parseFloat(valStr);
+    if (isNaN(val)) {
+      nanEl.classList.add("selected");
+    } else if (val === 0) {
+      zeroEl.classList.add("selected");
+    } else if (val > 0) {
+      posEl.classList.add("selected");
+    } else if (val < 0) {
+      negEl.classList.add("selected");
+    } else {
+      console.error("This should never happen. Investigate");
+    }
+  }
 });
 
-numEl.addEventListener("keyup", () => {
-  nanEl.style.backgroundColor = "rgba(255, 53, 22, 0.5)";
-});
+// On first load, make sure inputEl has selected class
+inputEl.classList.add("selected");
